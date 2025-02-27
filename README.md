39b57e2585f4b9f08a83a8348d6bd04877537615
alfonso.lorenzana@gmail.com <alfonso.lorenzana@gmail.com>
2025-02-27 14:35:32 -0500

    fixing README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# MONGOOSE_AL
+
+#### MongoDB Compass, was used to have a localhost DB, which is turn is used by Insomnia.
+#### Since this is NoSQL working format, it doesn't have postgreSQL.
+
+#### 
--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 const router = Router();
-import videoRoutes from './videoRoutes.js';
+import videoRoutes from './thoughtController.js';
 import userRoutes from './userRoutes.js';
 
 router.use('/videos', videoRoutes);
